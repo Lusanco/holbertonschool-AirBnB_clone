ad404d95433a58149389a26ec13b638883accde4
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-02-19 18:45:18 -0400

    code/test - base_model and test_ unittest

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+"""
+Module: base_model
+Descri: class BaseModel that defines all
+common attributes/methods for other classes
+Author: Livanhernandez, Lusanco
+"""
+
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """BaseModel Class"""
+
+    def __init__(self, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return "[{}] ({}) {}".format("BaseModel", self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        result = self.__dict__.copy()
+        result["__class__"] = self.__class__.__name__
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        return result
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_id(self):
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertIsInstance(b1, BaseModel)
+        self.assertTrue(hasattr(b1, "id"))
+        self.assertNotEqual(b1.id, b2.id)
+        self.assertIsInstance(b1.id, str)
+
+    def test_str(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1.__str__, str)
