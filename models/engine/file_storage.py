ebf4d08d6211ab191ac34f21c10328e120af97d6
Lusanco <lasc1026@gmail.com>
2024-02-20 15:23:55 -0400

    code - wip in other pc

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+"""
+Module: file_storage
+Descri: class FileStorage that serializes
+instances to a JSON file and deserializes
+JSON file to instances
+Author: Livanhernandez, Lusanco
+"""
+
+
+import json
+from os.path import isfile
+
+
+class FileStorage:
+    __file__path = __name__ + ".json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        return
+    
+    def save(self):
+        return
+    
+    def reload(self):
+        return
