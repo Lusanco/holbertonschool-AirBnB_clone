88e4929ed7d03bf6cfdbbd8a5675faf8adf33ba6
Livanhernandez <livanhernandez9@gmail.com>
2024-02-22 19:57:10 -0400

    console

--- console.py
+++ console.py
@@ -10,7 +10,6 @@ Author: Livanhernandez, Lusanco
 
 import cmd
 import json
-import os
 from models import storage
 from models.base_model import BaseModel
 
@@ -102,18 +101,34 @@ class HBNBCommand(cmd.Cmd):
             storage.delete(instance)
             print("** Instance deleted **")
 
+    def all(self, class_name=None):
+        if class_name is None:
+            return self.__objects
+        else:
+                filtered_objects = {}
+                for key, obj in self.__objects.items():
+                    if type(obj).__name__ == class_name:
+                        filtered_objects[key] = obj
+                return filtered_objects
+
     def do_all(self, args):
         """Prints all str representations of all instance."""
         class_name = None if not args else args.split()[0]
 
-        filtered_instances = storage.all() if not class_name else storage.all(class_name)
-
-        instance_string = []
-
-        for instance in filtered_instances.value():
-            instance_string.append(str(instance))
+        if class_name and class_name not in storage.all().keys():
+            print("** class doesn't exists **")
+            return
         
-        print("\n".join(instance_string))
+        instance_strings = []
+
+        for key, instance in storage.all().items():
+            if not class_name or type(instance).__name__ == class_name:
+                instance_strings.append(str(instance))
+
+        if instance_strings:
+            print("\n".join(instance_strings))
+        else:
+            print("** no instance found **")
 
     def do_update(self, args):
         """Updates an instance based on the class name, ID, attribute
