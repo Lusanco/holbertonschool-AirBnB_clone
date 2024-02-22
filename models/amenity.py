16cf4b541ad946855ab10ac5115b5855700ec278
Lusanco <lusanco@gmail.com>
2024-02-22 10:24:46 -0400

    code - amenity, city, place, review, state

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""
+Module: amenity
+Descri: class Amenity that inherits from BaseModel
+Author: Livanhernandez, Lusanco
+"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""
+Module: state
+Descri: class State that inherits from BaseModel
+Author: Livanhernandez, Lusanco
+"""
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    name = ""
