64884510156b11f1b7c33d28d65e7c8a183db796
Lusanco <lusanco@gmail.com>
2024-02-22 10:55:09 -0400

    unittest - amenity, city

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+import unittest
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+class TestAmenity(unittest.TestCase):
+
+    def test_name(self):
+        a = Amenity()
+        a.name = "amenity name"
+        self.assertEqual(a, str)
+
+    def test_id(self):
+        a1 = Amenity()
+        a2 = Amenity()
+        self.assertIsInstance(a1, BaseModel)
+        self.assertTrue(hasattr(a1, "id"))
+        self.assertNotEqual(a1.id, a2.id)
+        self.assertIsInstance(a1.id, str)
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+    
+    def test_stateid_name(self):
+        c = City()
+        c.state_id = "123"
+        c.name = "city"
+        self.assertEqual(c.state_id, str)
+        self.assertEqual(c.name, str)
+
+    def test_id(self):
+        c1 = City()
+        c2 = City()
+        self.assertIsInstance(c1, BaseModel)
+        self.assertTrue(hasattr(c1, "id"))
+        self.assertNotEqual(c1.id, c2.id)
+        self.assertIsInstance(c1.id, str)
