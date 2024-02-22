a26a92ec10a05f521761752168cf6d7a38de1492
Lusanco <lusanco@gmail.com>
2024-02-22 11:07:53 -0400

    unittest - review, state

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+
+class TestReview(unittest.TestCase):
+    
+    def test_placeuserid_text(self):
+        r = Review()
+        r.place_id = "Somewhere"
+        r.user_id = "Review"
+        r.text = "Reviewing"
+        self.assertEqual(r.place_id, str)
+        self.assertEqual(r.user_id, str)
+        self.assertEqual(r.text, str)
+
+    def test_id(self):
+        r1 = Review()
+        r2 = Review()
+        self.assertIsInstance(r1, BaseModel)
+        self.assertTrue(hasattr(r1, "id"))
+        self.assertNotEqual(r1.id, r2.id)
+        self.assertIsInstance(r1.id, str)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+import unittest
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    
+    def test_name(self):
+        s = State()
+        s.name = "State"
+        self.assertEqual(s.name, str)
+
+    def test_id(self):
+        s1 = State()
+        s2 = State()
+        self.assertIsInstance(s1, BaseModel)
+        self.assertTrue(hasattr(s1, "id"))
+        self.assertNotEqual(s1.id, s2.id)
+        self.assertIsInstance(s1.id, str)
