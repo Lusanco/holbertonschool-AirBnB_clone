9e9fdeb80270dd4fc9d383e9e8f33c2d1fb2fbd2
Livanhernandez <livanhernandez9@gmail.com>
2024-02-26 20:16:28 -0400

    fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,15 +16,16 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def __init__(self):
+        pass
     def all(self):
         """Return objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Saves new obj on dict"""
-        name_class = obj.__class__.__name__
-        key_obj = str(name_class = "." + obj.id)
-        self.__objects[key_obj] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves JSON dict"""
